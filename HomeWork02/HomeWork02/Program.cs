8bdcc8527d8ad07a2d8c51c16ea92374336e46df
Xiangwei Li <jobs4ever@me.com>
2018-09-12 22:32:21 -0500

    HomeWork02

---
+++ HomeWork02/HomeWork02/Program.cs
@@ -0,0 +1,28 @@
+﻿
+/*//////////////////////////////////////////////////////////////////////////////////////////////
+HomeWork #2: Your program should take an array of strings and check each element for the name “Waldo”. 
+If you find “Waldo” print out a message stating that you have found Waldo and at which position you found it. 
+
+Title:				Home Work #2
+Writeen by:			Shawn/Xiangwei Li
+Writeen for:		COMP 494A C#
+Date				09/12/2018
+Enviorment:			VS 2017
+
+///////////////////////////////////////////////////////////////////////////////////////////////*/
+///
+using System;
+
+namespace HomeWork02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Tom", "Bob", "Dana", "Julie", "Sarah", "Fred", "Waldo", "Jenny", "Cathy" };
+            var target = "Waldo";
+            int pos = Array.IndexOf(names, target); // find the position by IndexOf method
+            Console.WriteLine("Waldo has been found at position: " + pos); 
+        }
+    }
+}
