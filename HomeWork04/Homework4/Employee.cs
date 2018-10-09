b481f2bd16e985d1df2e00e3d4df1395c4e4b480
li shawn <think4ever@outlook.com>
2018-10-08 21:25:35 -0500

    Homework4

---
+++ HomeWork04/Homework4/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Homework4
+{
+        public class Employee
+        {
+            public Employee()
+            {
+                FirstName = "Default first name";
+                LastName = "Default last name";
+                EmployeeCode = "0";
+                DateHired = new DateTime();
+            }
+            // constructors
+            public Employee(string fname, string lname, string ecode, DateTime htime)
+            {
+                this.FirstName = fname;
+                this.LastName = lname;
+                this.EmployeeCode = ecode;
+                this.DateHired = htime;
+            }
+
+            // methods
+            public void WriteToConsole()
+            {
+                Console.WriteLine("==============================");
+                Console.WriteLine("EMPLOYEE NAME: " + FirstName + " " + LastName);
+                Console.WriteLine("==============================");
+                Console.WriteLine("EMPLOYEE ID: " + EmployeeCode);
+                Console.WriteLine("==============================");
+                Console.WriteLine("HIRED DATE: " + DateHired);
+                Console.WriteLine("==============================");
+
+            }
+
+            // Properties
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string EmployeeCode { get; set; }
+            public DateTime DateHired { get; set; }
+
+        }
+    
+}
