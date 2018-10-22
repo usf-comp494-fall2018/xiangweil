aeca62456759fef5911c6116be18bc3a5f6ed217
Xiangwei Li <jobs4ever@me.com>
2018-10-22 17:56:43 -0500

    Midterm1.0

---
+++ Homework04_V1.0/Homework04/PartTimeEmployee.cs
@@ -0,0 +1,33 @@
+﻿// Written by: Shawn
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework04
+{
+    public class PartTimeEmployee : Employee
+    {
+        // Default constructors
+        public PartTimeEmployee() { }
+        // constructors
+        public PartTimeEmployee(string fname, string lname, string ecode, DateTime htime, int hrate)
+        {
+            this.FirstName = fname;
+            this.LastName = lname;
+            this.EmployeeCode = ecode;
+            this.DateHired = htime;
+            this.HourlyRate = hrate;
+        }
+
+        // methods
+        public new void WriteToConsole()
+        {
+            base.WriteToConsole();
+            Console.WriteLine("HOURLY RATE: " + HourlyRate);
+            Console.WriteLine("==============================");
+        }
+
+        // Properties
+        public int HourlyRate { get; set; }
+    }
+}
