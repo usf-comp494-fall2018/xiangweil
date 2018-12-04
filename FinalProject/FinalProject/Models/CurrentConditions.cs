b606b8d7b84aa3f370ee76b33178f86638c87f3d
li shawn <think4ever@outlook.com>
2018-12-03 19:26:30 -0600

    demo

---
+++ FinalProject/FinalProject/Models/CurrentConditions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalProjectService.Models
+{
+    public partial class CurrentConditions
+    {
+        public long Id { get; set; }
+        public string ObservationTime { get; set; }
+        public string Weather { get; set; }
+        public double TempF { get; set; }
+        public string RelativeHumidity { get; set; }
+        public string WindDir { get; set; }
+        public double WindMph { get; set; }
+        public string Lastcheck { get; set; }
+    }
+}
---
+++ FinalProject/FinalProject/Models/ThreeDayForecast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalProjectService.Models
+{
+    public partial class ThreeDayForecast
+    {
+        public long Id { get; set; }
+        public long Period { get; set; }
+        public string Icon { get; set; }
+        public string Title { get; set; }
+        public string Fcttext { get; set; }
+        public string Lastcheck { get; set; }
+    }
+}
